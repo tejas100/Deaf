30b755ce9598919c86097b1ad9f54ff687437043
kishoresolanki <kishoreseervi643@gmail.com>
2019-10-25 15:12:06 +0530

    change in number

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var touch_count =0;
 
 
 function myFunction() {
-    inc()
+   touch_count++;
+   if(touch_count>10){
+     touch_count=0;
+   }
     document.getElementById("demo1").innerHTML = touch_count;
   }
